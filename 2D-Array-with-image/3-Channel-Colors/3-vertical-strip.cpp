7c72a3858296df66f98633052414bd129eca3784
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-09-25 21:30:11 +0500

    3 vertical strips

---
+++ 2D-Array-with-image/3-Channel-Colors/3-vertical-strip.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+int main()
+{
+    int h = 300, w = 300;
+
+    ofstream file("output.ppm");
+    file << "P3\n" << "# nothing....\n";
+    file << w << " " << h << "\n" << 255 << endl;
+
+    for (int i = 0; i < h; i++)
+    {
+        for (int j = 0; j < 100; j++) file << "255 0 0 ";
+        for (int j = 0; j < 100; j++) file << "0 255 0 ";
+        for (int j = 0; j < 100; j++) file << "0 0 255 ";
+    }
+    file.close();
+    return 0;
+}
