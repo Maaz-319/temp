21f40660d94da0c3ce8e01ec0ea6717405011a6c
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-10-06 19:58:37 +0500

    two image halves with colorful diagnols but opposite directions

---
+++ OOP_Practice_Task_5/colourful_diagnols_2_halves.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+    srand(time(NULL));
+    ofstream file("colorful_diagnols.ppm");
+
+    file << "P3\n200 200\n255\n";
+    int diagnols = 200 + 200 - 1;
+    int R[diagnols], G[diagnols], B[diagnols];
+    for (int d = 0; d < diagnols; ++d)
+    {
+        R[d] = rand() % 256;
+        G[d] = rand() % 256;
+        B[d] = rand() % 256;
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        for (int j = 0; j < 200; j++)
+        {
+            int idx = j - i + (199);
+            file << R[idx] << " " << G[idx] << " " << B[idx] << " ";
+        }
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        for (int j = 0; j < 200; j++)
+        {
+            int idx = i + j + (199);
+            file << R[idx] << " " << G[idx] << " " << B[idx] << " ";
+        }
+    }
+    
+    file.close();
+}
