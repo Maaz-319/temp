067679ce3c04b91c33a2e2b3bb1cdaf6407a3342
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-10-08 18:30:04 +0500

    copy one array to other for min size only

--- OOP_Practice_Task_6/header_file/array_helper.h
+++ OOP_Practice_Task_6/header_file/array_helper.h
@@ -14,3 +14,9 @@ int *create_array(int s)
 }
 
 int *delete_array(int *ptr, int *size) { *size = 0; delete[] ptr; return nullptr; }
+
+void copy_array(const int *source, int *dest, int source_size, int dest_size)
+{
+    if (source_size <= 0 || dest_size <= 0) return;
+    for (int i = 0; i < std::min(source_size, dest_size); i++) dest[i] = source[i];
+}
