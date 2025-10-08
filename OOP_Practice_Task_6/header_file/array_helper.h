b25b4858c52c3c545aa83b0fb9510cf1fddde862
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-10-08 18:30:19 +0500

    sort an array using selection sort

--- OOP_Practice_Task_6/header_file/array_helper.h
+++ OOP_Practice_Task_6/header_file/array_helper.h
@@ -20,3 +20,14 @@ void copy_array(const int *source, int *dest, int source_size, int dest_size)
     if (source_size <= 0 || dest_size <= 0) return;
     for (int i = 0; i < std::min(source_size, dest_size); i++) dest[i] = source[i];
 }
+
+void sort_array(int *arr, int size)
+{
+    if (size <= 0) return;
+    for (int i = 0; i < size - 1; i++)
+    {
+        int min_idx = i;
+        for (int j = i + 1; j < size; ++j) if (arr[j] < arr[min_idx]) min_idx = j;
+        std::swap(arr[i], arr[min_idx]);
+    }
+}
