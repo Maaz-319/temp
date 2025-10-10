c63c4669b85ef2c6daebe283e416475b16510f05
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-10-10 16:16:25 +0500

    parse a packet using hexa packets

---
+++ OOP_Practice_Task_6/network_package_parser.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <iomanip> // for hex and setw
+
+using namespace std;
+
+void parsePacket(const unsigned char *buffer)
+{
+    cout << "Packet Type: 0x" << hex << setw(4) << setfill('0') << ((buffer[0] << 8) | buffer[1]);
+
+    cout << "\nTimestamp: " << dec << ((buffer[2] << 24) | (buffer[3] << 16) | (buffer[4] << 8) | (buffer[5]));
+
+    cout << "\nPayload: ";
+    for (int i = 6; i < 14; i++)
+    {
+        cout << hex << setw(2) << setfill('0') << (int)buffer[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    unsigned char buffer[14] = {0x01, 0x02, 0x00, 0x00, 0x00, 0x10, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x11, 0x22};
+    parsePacket(buffer);
+    return 0;
+}
