b537792d3c940698808b0c4b2ce4b9f5ffbefdad
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-10-13 16:16:09 +0500

    print all arguments passed to program file

---
+++ arg_to_main/print_all_args.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    for (int i = 1; i < argc; ++i)
+    {
+        std::cout << "Arg - " << i << ": " << argv[i] << '\n';
+    }
+
+    return 0;
+}
