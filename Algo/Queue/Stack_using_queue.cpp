05ba2ea7266353adafd4895268539d5dbd368338
Maaz Bin Asif <maazbinaasif123@outlook.com>
2026-03-11 11:55:47 +0500

    stack using queue

---
+++ Algo/Queue/Stack_using_queue.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+class MyStack
+{
+    queue<int> q;
+
+public:
+    MyStack()
+    {
+    }
+
+    void push(int x)
+    {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++)
+        {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop()
+    {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+
+    int top()
+    {
+        return q.front();
+    }
+
+    bool empty()
+    {
+        return q.empty();
+    }
+};
