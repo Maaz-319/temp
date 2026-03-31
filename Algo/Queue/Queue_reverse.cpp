09534831a54c6b4724d921b193921bfbe9fe049f
Maaz Bin Asif <maazbinaasif123@outlook.com>
2026-03-31 19:39:27 +0500

    reverse queue using recursion

---
+++ Algo/Queue/Queue_reverse.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+void reverse(queue<int> &q)
+{
+    if (q.empty()) return;
+    int p = q.front();
+    q.pop();
+    reverse(q);
+    q.push(p);
+}
+
+int main()
+{
+    queue<int> q;
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    q.push(7);
+    reverse(q);
+    while (!q.empty())
+    {
+        cout << q.front() << " ";
+        q.pop();
+    }
+    return 0;
+}
