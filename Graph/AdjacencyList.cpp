2eea1da9b69fd2194667e627f1e99e09544e6475
Maaz Bin Asif <maazbinaasif123@outlook.com>
2026-05-16 19:58:57 +0500

    dfs on adjacency list

--- Graph/AdjacencyList.cpp
+++ Graph/AdjacencyList.cpp
@@ -2,6 +2,16 @@
 #include <vector>
 using namespace std;
 
+void dfs(vector<vector<int>> &adjList, int s, vector<bool> &visited)
+{
+    visited[s] = true;
+    cout << s<< " ";
+
+    for (int i : adjList[s])
+        if (!visited[i])
+            dfs(adjList, i, visited);
+}
+
 vector<vector<int>> Graph(int vertices, vector<vector<int>> &edges)
 {
     vector<vector<int>> List(vertices);
@@ -20,8 +30,9 @@ int main()
     int nodes = 5;
 
     vector<vector<int>> edges = {{0, 1}, {0, 2}, {4, 3}, {2, 4}, {3, 0}};
-
     vector<vector<int>> AdjacencyList = Graph(nodes, edges);
+    vector<bool> visited(nodes, false);
+    vector<int> res(nodes);
 
     for (int i = 0; i < nodes; i++)
     {
@@ -30,6 +41,9 @@ int main()
             cout << x << " ";
         cout << endl;
     }
+
+    cout << "\n\nDFS:\n";
+    dfs(edges, 0, visited);
 
     return 0;
 }
